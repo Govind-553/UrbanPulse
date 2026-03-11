3a01bcb1c7f5eeb214b3634f356c906b77d4ce70
Govind Choudhari <choudharygovind553@gmail.com>
2026-03-12 03:52:43 +0530

    Minor tweaks added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/db');
 const logger = require('./config/logger');
 
 // Load env vars
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 // Connect to database
 connectDB();
